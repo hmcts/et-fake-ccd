4d57316b16a495e8924cc227e1ff791be3d355c4
Gary Taylor <gary.taylor@hismessages.com>
2019-07-03 10:41:33 +0100

    Removed hard coded localhost:8080

--- lib/et_fake_ccd/service/ui_app.rb
+++ lib/et_fake_ccd/service/ui_app.rb
@@ -2,6 +2,7 @@ require 'roda'
 require 'json'
 require 'et_fake_ccd/commands'
 require 'et_fake_ccd/auth_service'
+require 'addressable/uri'
 module EtFakeCcd
   module Service
     class UiApp < Roda
@@ -19,16 +20,22 @@ module EtFakeCcd
 
       def config_result
         j = {
-            "login_url": "http://localhost:8080/authentication-web/login",
-            "logout_url": "http://localhost:8080/api-gateway/logout",
-            "api_url": "http://localhost:8080/api-gateway/aggregated",
-            "case_data_url": "http://localhost:8080/api-gateway/data",
-            "document_management_url": "http://localhost:8080/api-gateway/documents",
+            "login_url": url_for("/authentication-web/login"),
+            "logout_url": url_for("http://localhost:8080/api-gateway/logout"),
+            "api_url": url_for("http://localhost:8080/api-gateway/aggregated"),
+            "case_data_url": url_for("http://localhost:8080/api-gateway/data"),
+            "document_management_url": url_for("http://localhost:8080/api-gateway/documents"),
             "pagination_page_size": 25,
-            "oauth2_token_endpoint_url": "http://localhost:8080/api-gateway/oauth2",
-            "oauth2_client_id": "ccd_gateway"
+            "oauth2_token_endpoint_url": url_for("http://localhost:8080/api-gateway/oauth2"),
+            "oauth2_client_id": url_for("ccd_gateway")
         }
         JSON.generate(j)
+      end
+
+      def url_for(path)
+        uri = Addressable::URI.parse(request.url)
+        uri.path = path
+        uri.to_s
       end
     end
   end
