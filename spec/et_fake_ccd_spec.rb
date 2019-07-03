168c62920fd21130a86bfb7080ab581c2354c500
Gary Taylor <gary.taylor@hismessages.com>
2019-07-03 07:11:02 +0100

    Added publishing to rubygems

--- spec/et_fake_ccd_spec.rb
+++ spec/et_fake_ccd_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe EtFakeCcd do
   it "has a version number" do
     expect(EtFakeCcd::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
