d0eb0c52e5cc948047232f577f84e30e9311d689
Gary Taylor <gary.taylor@hismessages.com>
2019-07-24 09:56:17 +0100

    Limited to single thread for now - as in memory implementation is not thread safe

--- lib/et_fake_ccd/cli/root.rb
+++ lib/et_fake_ccd/cli/root.rb
@@ -10,7 +10,7 @@ module EtFakeCcd
       desc "start", "Run multiple services on one port"
       method_option :port, type: :numeric, default: 8080
       def start
-        Rack::Server.start app: EtFakeCcd::RootApp, Port: options.port
+        Rack::Server.start app: EtFakeCcd::RootApp, Port: options.port, Threads: '1:1', server: 'puma'
       end
     end
   end
