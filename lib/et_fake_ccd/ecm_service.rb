27144d391f7a30da99129bc27aac4c2858227663
Gary Taylor <gary.taylor@hismessages.com>
2020-04-13 15:13:09 +0100

    1.0.5 /ecm/startMultiple now returning start of range correctly

--- lib/et_fake_ccd/ecm_service.rb
+++ lib/et_fake_ccd/ecm_service.rb
@@ -18,8 +18,7 @@ module EtFakeCcd
     end
 
     def call
-      adapter.reserve_reference_numbers_for(command.case_type_id, quantity: command.case_ref_number_count)
-      start_reference = adapter.last_case_reference(command.case_type_id) - command.case_ref_number_count
+      start_reference = adapter.reserve_reference_numbers_for(command.case_type_id, quantity: command.case_ref_number_count)
       formatted_ref = "#{office_code_for(command.case_type_id)}#{start_reference.to_s.rjust(5, '0')}/#{Time.now.year}"
       formatted_multiple = "#{office_code_for(command.case_type_id)}#{adapter.build_multiple_reference(command.case_type_id).to_s.rjust(5, '0')}/#{Time.now.year}"
       Response.new(command.case_ref_number_count, formatted_ref, formatted_multiple).freeze
@@ -46,18 +45,12 @@ module EtFakeCcd
         self.data = {}
       end
 
-      def last_case_reference(case_type_id)
-        last_ref = data.dig(case_type_id, :case_reference)
-        raise "Case reference not generated for #{case_type_id}" if last_ref.nil?
-
-        last_ref
-      end
-
-
       def reserve_reference_numbers_for(case_type_id, quantity:)
         data[case_type_id] ||= {}
-        data[case_type_id][:case_reference] ||= 0
-        data[case_type_id][:case_reference] += quantity
+        data[case_type_id][:next_case_reference] ||= 1
+        start = data[case_type_id][:next_case_reference]
+        data[case_type_id][:next_case_reference] += quantity
+        start
       end
 
       def build_multiple_reference(case_type_id)
