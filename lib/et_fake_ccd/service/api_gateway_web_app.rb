5c6423b1099c18d7bdcc6c6ff6f393df0f7c61de
garytaylor <gary.taylor@hismessages.com>
2023-03-29 07:11:19 +0100

    (RST-5253) Allow creating of case from the test suite

--- lib/et_fake_ccd/service/api_gateway_web_app.rb
+++ lib/et_fake_ccd/service/api_gateway_web_app.rb
@@ -25,6 +25,29 @@ module EtFakeCcd
               list = DataStoreService.list(jid: jid, ctid: ctid, filters: filters, page: page, sort_direction: sort_direction, page_size: 25)
               cases_response(list, uid, jid, ctid)
             end
+
+            r.post do
+              json = JSON.parse(r.body.read)
+
+              command = case json.dig('event', 'id')
+              when 'initiateCase' then ::EtFakeCcd::Command::CreateCaseCommand.from_json json
+                        when 'createMultiple' then ::EtFakeCcd::Command::CreateMultipleCaseCommand.from_json json
+                        else
+                          r.halt 400, unknown_event_error_for(r)
+                        end
+              if command.valid?
+                id = ::EtFakeCcd::DataStoreService.store_case_data(command.data, jid: jid, ctid: ctid)
+                case_created_response(id, uid, jid, ctid)
+              else
+                r.halt 422, render_error_for(command, r)
+              end
+
+            end
+          end
+          r.is "caseworkers", String, "jurisdictions", String, "case-types", String, "event-triggers", "initiateCase", "token" do |uid, jid, ctid|
+            r.get do
+              initiate_case(uid, jid, ctid)
+            end
           end
         end
         r.on "data" do
@@ -108,6 +131,59 @@ module EtFakeCcd
             "_links": {}
         }
         JSON.generate j
+      end
+
+      def initiate_case(uid, jid, ctid)
+        j = {
+            "token": "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJvZDRwZ3NhbDQwcTdndHI0Y2F1bmVmZGU5aSIsInN1YiI6IjIyIiwiaWF0IjoxNTYxOTY2NzM1LCJldmVudC1pZCI6ImluaXRpYXRlQ2FzZSIsImNhc2UtdHlwZS1pZCI6IkVtcFRyaWJfTVZQXzEuMF9NYW5jIiwianVyaXNkaWN0aW9uLWlkIjoiRU1QTE9ZTUVOVCIsImNhc2UtdmVyc2lvbiI6ImJmMjFhOWU4ZmJjNWEzODQ2ZmIwNWI0ZmEwODU5ZTA5MTdiMjIwMmYifQ.u-OfexKFu52uvSgTNVHJ5kUQ9KTZGClRIRnGXRPSmGY",
+            "case_details": {
+                "id": nil,
+                "jurisdiction": jid,
+                "state": nil,
+                "case_type_id": ctid,
+                "created_date": nil,
+                "last_modified": nil,
+                "security_classification": nil,
+                "case_data": {},
+                "data_classification": {},
+                "after_submit_callback_response": nil,
+                "callback_response_status_code": nil,
+                "callback_response_status": nil,
+                "delete_draft_response_status_code": nil,
+                "delete_draft_response_status": nil,
+                "security_classifications": {}
+            },
+            "event_id": "initiateCase"
+        }
+        JSON.generate(j)
+      end
+
+      def case_created_response(id, uid, jid, ctid)
+        j = case_hash(ctid, id, jid)
+        JSON.generate(j)
+      end
+
+      def render_error_for(command, request)
+        j = {
+            "exception": "uk.gov.hmcts.ccd.endpoint.exceptions.CaseValidationException",
+            "timestamp": "2019-07-01T16:02:28.045",
+            "status": 422,
+            "error": "Unprocessable Entity",
+            "message": "Case data validation failed",
+            "path": request.path,
+            "details": {
+                "field_errors": command.errors.details[:data].map {|e| e[:field_error]}
+            },
+            "callbackErrors": nil,
+            "callbackWarnings": nil
+        }
+
+        JSON.generate(j)
+      end
+
+      def unknown_event_error_for(r)
+        j = {"timestamp":"2019-07-01T07:46:35.405+0000","status":400,"error":"Unknown event","message":"Unknown event","path": r.path}
+        JSON.generate(j)
       end
     end
   end
--- lib/et_fake_ccd/version.rb
+++ lib/et_fake_ccd/version.rb
@@ -1,3 +1,3 @@
 module EtFakeCcd
-  VERSION = "2.0.0"
+  VERSION = "2.1.0"
 end
